3fb70f7e524a333b8c4375209d262598c3c53b9d
Anuj <anujkumarnath@gmail.com>
2024-05-19 07:40:27 +0000

    setup basic client side router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import { Switch, Route, Link } from 'react-router-dom';
 import ProductManager from './features/products/ProductManager';
 
+function Home () {
+	return (
+		<>
+			<h1>Home</h1>
+			<Link to='/products'>Products</Link>
+		</>
+	);
+}
+
 function App() {
-	return <ProductManager />;
+	return (
+		<Switch>
+			<Route path="/products">
+				<ProductManager />
+			</Route>
+			<Route path="/">
+				<Home />
+			</Route>
+		</Switch>
+	);
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
 
@@ -7,9 +8,11 @@ import { store } from './store';
 import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+	<React.StrictMode>
 		<Provider store={store}>
-			<App />
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
 		</Provider>
-  </React.StrictMode>,
+	</React.StrictMode>,
 );
